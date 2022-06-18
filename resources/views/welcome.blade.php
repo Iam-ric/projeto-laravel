1ca1b7a7db71188fb57386e43de3ebf18a81cc94
iam <iam.henrique.ads@gmail.com>
2022-06-18 20:47:18 -0300

    home-css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,28 @@
 
 @section('content')
 
-@foreach($books as $book)
- <p>{{ $book->name }} -- {{$book->price}}</p>
-@endforeach
+<div id="search-container" class="col-md-12">
+   <h1>search for books</h1> 
+   <form action="">
+    <input type="text" id="search" name="search" class="form-control" placeholder="Search ....">
+   </form>
+</div>
+<div id="books-container" class="col-md-12">
+    <h2>Other Books</h2>
+    <p class="subtitle">Find more books</p>
+    <div id="cards-container" class="row">
+        @foreach($books as $book)
+        <div class="card col-md-3">
+            <img src="/img/kotlin.jpg" alt="{{$book -> title}}">
+           <div class="card-body">
+               <p class="card-date">18/06/2022</p>
+                <h5 class="card-name">{{$book->name}}></h5>
+                <p class="card-status">status:{{$book->status}}</p>
+                <a href="a" class="btn btn-primary">learn more</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
    
 @endsection
